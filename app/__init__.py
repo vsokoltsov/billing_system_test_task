ec3a059404fb061234af901e78b70d51d13b5649
Vadim Sokoltsov <vforvad@gmail.com>
2021-01-30 22:33:45 +0300

    Remove tags from api

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from app.api import users, wallet
 from app.db import db
 
 app = FastAPI(title="Billing system sample")
-app.include_router(users.router, prefix="/api", tags=["api"])
-app.include_router(wallet.router, prefix="/api", tags=["api"])
+app.include_router(users.router, prefix="/api")
+app.include_router(wallet.router, prefix="/api")
 
 @app.on_event("startup")
 async def startup():
