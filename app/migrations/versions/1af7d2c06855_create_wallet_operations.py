d14d9bff115c373c571836b7a609131a963444d8
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-23 01:41:13 +0300

    Move migrations into the app folder

---
+++ app/migrations/versions/1af7d2c06855_create_wallet_operations.py
@@ -0,0 +1,33 @@
+"""create wallet operations
+
+Revision ID: 1af7d2c06855
+Revises: 8697d766eeb4
+Create Date: 2021-01-31 20:29:13.453079
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import CheckConstraint
+from sqlalchemy.dialects.postgresql import NUMERIC
+
+# revision identifiers, used by Alembic.
+revision = "1af7d2c06855"
+down_revision = "8697d766eeb4"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "wallet_operations",
+        sa.Column("id", sa.Integer, primary_key=True, nullable=False),
+        sa.Column("operation", sa.String),
+        sa.Column("wallet_from", sa.Integer, nullable=True),
+        sa.Column("wallet_to", sa.Integer, nullable=True),
+        sa.Column("amount", NUMERIC(10, 2), nullable=False, server_default="0"),
+        CheckConstraint("amount >= 0", name="operations_positive_amount"),
+    )
+
+
+def downgrade():
+    op.drop_table("wallet_operations")
--- app/migrations/versions/7b1f4f065670_create_users_table.py
+++ app/migrations/versions/7b1f4f065670_create_users_table.py
@@ -6,11 +6,10 @@ Create Date: 2021-01-30 16:38:42.539268
 
 """
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '7b1f4f065670'
+revision = "7b1f4f065670"
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -19,9 +18,9 @@ depends_on = None
 def upgrade():
     op.create_table(
         "users",
-        sa.Column('id', sa.Integer, primary_key=True, nullable=False),
-        sa.Column('email', sa.String, nullable=False),
-        sa.UniqueConstraint("email", name="unique_email")
+        sa.Column("id", sa.Integer, primary_key=True, nullable=False),
+        sa.Column("email", sa.String, nullable=False),
+        sa.UniqueConstraint("email", name="unique_email"),
     )
 
 
