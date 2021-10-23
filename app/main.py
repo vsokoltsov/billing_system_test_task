fa694a986dc0a34cf6308f66f23211e295ad80d2
Vadim Sokoltsov <vforvad@gmail.com>
2021-10-24 00:39:03 +0300

    Add diffrent way of server running

--- app/main.py
+++ app/main.py
@@ -1,34 +1,38 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from typing import List
 
 from app import settings
 from app.api import users_routes, wallets_routes
 from app.db import db
-
-app = FastAPI(title="Billing system sample")
-
-app.add_event_handler("startup", db.connect)
-app.add_event_handler("shutdown", db.disconnect)
-
-app.include_router(users_routes, prefix="/api")
-app.include_router(wallets_routes, prefix="/api")
-
-# @app.on_event("startup")
-# async def startup():
-#     """ Connect to the database. """
-
-#     await db.connect()
+from databases import Database
 
 
-# @app.on_event("shutdown")
-# async def shutdown():
-#     """ Close connection to the database. """
+def init_app(db: Database, routes: List[APIRouter]) -> FastAPI:
+    """Initialize application parameters."""
 
-#     await db.disconnect()
+    app = FastAPI(title="Billing system sample")
+    app.add_event_handler("startup", db.connect)
+    app.add_event_handler("shutdown", db.disconnect)
+    for route in routes:
+        app.include_router(route, prefix="/api")
+    return app
+
+
+def run_app():
+    app = init_app(
+        db=db,
+        routes=[
+            users_routes,
+            wallets_routes
+        ]
+    )
+    return app
+
 
 if __name__ == "__main__":
     uvicorn.run(
-        app="app.main:app",
+        app="app.main:run_app",
         host=settings.APP_HOST,
         port=settings.APP_PORT,
         loop="uvloop",
